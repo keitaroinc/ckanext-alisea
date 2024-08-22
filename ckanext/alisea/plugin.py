11f1f8365437e574425d54374ac04f57a3969808
Filip Mitrovski <filip.mitrovski@keitaro.com>
2024-08-22 15:14:57 +0200

    Google tag added

--- ckanext/alisea/plugin.py
+++ ckanext/alisea/plugin.py
@@ -1,9 +1,12 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
+from ckanext.alisea import helpers as h
+
 
 class AliseaPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.ITemplateHelpers)
     
 
     # IConfigurer
@@ -12,5 +15,12 @@ class AliseaPlugin(plugins.SingletonPlugin):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("assets", "alisea")
+
+
+    # ITemplateHelpers
+    def get_helpers(self):
+        return {
+            'get_google_tag':h.get_google_tag
+        }
 
     
